6b4744ca91c403b61fe51f5076df62ed66b5b246
aorts <a.orts@highco-data.fr>
2018-01-24 09:14:32 +0100

    Preset orderBy rendering

--- assets/app.js
+++ assets/app.js
@@ -2,7 +2,11 @@ var table;
 var socket;
 
 $(document).ready(function () {
-    table = $('#chrono').DataTable();
+    table = $('#chrono').DataTable({
+        "order": [[ 1, "desc" ],[ 0, "asc" ]],
+        "lengthMenu": [[-1], ["All"]],
+        "dom": '<"wrapper"flip>t'
+    });
 
     socket = io.connect('http://localhost:8080');
     socket.on('init', function (data) {
@@ -17,7 +21,6 @@ $(document).ready(function () {
 var addRows = function (rows) {
     rows.forEach(function (value, index) {
         table.row.add([
-                value.id,
                 value.pilote,
                 value.tour,
                 value.temps
